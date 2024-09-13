59c774249c19cd7144be4cc7e51c6d3fb7e755d4
Sarah Mourlevat <sarah.mourlevat@gmail.com>
2024-09-13 16:58:58 +0200

    new modif

--- config/cors.php
+++ config/cors.php
@@ -3,13 +3,17 @@
 return [
     'paths' => ['api/*'],
     'allowed_methods' => ['*'],
-    'allowed_origins' => ['http://localhost:3000'], 
+    'allowed_origins' => [
+        'http://localhost:3000', 
+        'https://gestion-de-finances-front-end.onrender.com', 
+    ],
     'allowed_origins_patterns' => [],
     'allowed_headers' => ['*'],
     'exposed_headers' => [],
     'max_age' => 0,
     'supports_credentials' => true,
 ];
+
 
 
 
